24efe1ca3816a72437d62b6e8968a67e60cd8c4b
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2017-06-02 23:26:58 +0100

    adding js files

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyDwaRgyWVEMR1IeuzwYLhTZdmlvJnG1nfo",
+  authDomain: "react-firebase-c5630.firebaseapp.com",
+  databaseURL: "https://react-firebase-c5630.firebaseio.com",
+  projectId: "react-firebase-c5630",
+  storageBucket: "react-firebase-c5630.appspot.com",
+  messagingSenderId: "1036943659023"
+};
+firebase.initializeApp(config);
+
+export default firebase;
